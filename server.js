17acdc16ec6ace461c9967bd18a91ced096971ba
Noesis Mik <sharp7c0timesfour@gmail.com>
2016-09-12 13:57:16 +0900

    install template engine (EJS)

--- server.js
+++ server.js
@@ -2,23 +2,47 @@
 
 const Config = require('./config.js'); 
 const Hapi   = require('hapi');
+const Path   = require('path');
 
 // TODO : check Config is valid
 const server = new Hapi.Server();
 server.connection({ host: Config.host, port: Config.port });
 
-// routes.. 
+/** Install template engine **/
+server.register(require('vision'), (err) => {
+  
+  if (err) {
+    throw err;
+  }
+
+  server.views({
+    engines   : { ejs: require('ejs') },
+    relativeTo: __dirname,
+    path      : 'views'
+  });
+});
+
+/** routes **/
+
+// GET : index
+// desc - randering index page
 server.route({
   method: 'GET',
   path  : '/',
   handler (request, reply) {
-    reply('Hello, world!');
+    reply.view('index', { title: 'My home page' });
   }
 });
 
+
+///////////////////////////////////////////////////////
+////////////// some wall ~~~ //////////////////////////
+//////////////////////////////////////////////////////
+
+/** Running server **/
 server.start((err) => {
   if (err) {
-      throw err;
+    throw err;
   }
   console.log(`Server running at: ${server.info.uri}`);
 });
