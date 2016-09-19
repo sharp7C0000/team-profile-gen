f3f94e777a432dfefa3fa923d90dc12ff13122e8
Noesis Mik <sharp7c0timesfour@gmail.com>
2016-09-19 14:17:34 +0900

    신규 생성 페이지 top bar 마크업 작업

--- assets/scripts/new/components/nav.jsx
+++ assets/scripts/new/components/nav.jsx
@@ -2,16 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
+require("../../../scss/new/nav.scss");
+
 class Nav extends React.Component {
 
   render() {
-    return  <div className="nav">
-      <div className="nav-left">
-        <a className="nav-item is-brand" href="/">
-          Team Profile Page Generator
-        </a>
+    return  <div className="comp-nav">
+
+      <div className="hero is-primary is-medium">
+        <div className="hero-head">
+           <div className="nav">
+              <div className="container">
+                <div className="nav-left">
+                  <div className="nav-item">
+                    <div className="title is-4">
+                      <span className="icon">
+                        <i className="fa fa-magic"></i>
+                      </span>&nbsp;New Page
+                    </div>
+                  </div>
+                </div>
+
+                <div className="nav-right nav-menu">
+                  <a href="/" className="nav-item">
+                    <span className="icon is-small">
+                      <i className="fa fa-home"></i>
+                    </span>
+                    &nbsp;Home
+                  </a>
+                  {/* TODO */}
+                  <a href="#" className="nav-item">
+                    <span className="icon is-small">
+                      <i className="fa fa-question"></i>
+                    </span>
+                    &nbsp;Help
+                  </a>
+                </div>
+
+              </div>
+           </div>
+        </div>
       </div>
-    </div>  
+    </div>
   }
 }
 
