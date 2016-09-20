b98edc7924602a0ec2212be352fa7fda1a86ff0f
Noesis Mik <sharp7c0timesfour@gmail.com>
2016-09-20 15:28:35 +0900

    title 인풋 추가

--- assets/scripts/new/components/contents/title.jsx
+++ assets/scripts/new/components/contents/title.jsx
@@ -4,22 +4,90 @@ import { connect } from 'react-redux';
 
 class Title extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditing: false,
+      value    : null,
+      tempValue: null
+    };
+  }
+
+  changeTempValue (e) {
+    this.setState({tempValue: e.target.value});
+  }
+
+  enterEdit() {
+    this.setState({isEditing: true});
+  }
+
+  exitEdit(applyEdit = false) {
+    this.setState({isEditing: false});
+    if(applyEdit) {
+      this.setState({value: this.state.tempValue});
+    }
+  }
+
   render() {
     return  <div className="comp-title level">
       <div className="level-left">
         <div className="level-item">
-          <p className="title">Sales department 1 - Team A</p>
+
+          {(() => {
+            if(this.state.isEditing) {
+              return <p className="control">
+                <input className="input is-large" type="text" 
+                  onChange={this.changeTempValue.bind(this)} 
+                  defaultValue={this.state.value} 
+                  placeholder="Team Name"
+                />
+              </p>
+            } else {
+              return <p className={"title is-3 " + (!this.state.value ? 'placeholder' : '')}>
+                {(() => {
+                  if(this.state.value) {
+                    return this.state.value
+                  } else {
+                    return "Team Name"
+                  }
+                })()}
+              </p>
+            }
+          })()}
+
         </div>
       </div>
 
       <div className="level-right">
         <div className="level-item">
-          <button className="button is-small is-success is-outlined">
-            <span className="icon is-small">
-              <i className="fa fa-pencil"></i>
-            </span>
-            <span>edit</span>
-          </button>
+          {(() => {
+            if(this.state.isEditing) {
+              {/** edit mode **/}
+              return <span>
+                <button type="button" onClick={this.exitEdit.bind(this, true)} className="button is-small is-primary is-outlined">
+                  <span className="icon is-small">
+                    <i className="fa fa-check"></i>
+                  </span>
+                  <span>ok</span>
+                </button>&nbsp;
+                <button type="button" onClick={this.exitEdit.bind(this)} className="button is-small is-danger is-outlined">
+                  <span className="icon is-small">
+                    <i className="fa fa-times"></i>
+                  </span>
+                  <span>cancel</span>
+                </button>
+              </span>
+            } else {
+              {/** not edit mode **/}
+              return <button type="button" onClick={this.enterEdit.bind(this)} className="button is-small is-success is-outlined">
+                <span className="icon is-small">
+                  <i className="fa fa-pencil"></i>
+                </span>
+                <span>edit</span>
+              </button>
+            }
+          })()}
         </div>
       </div>
     </div>
