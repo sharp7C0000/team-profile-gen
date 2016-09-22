fbffd2d7dd494fd60531f645f6c2fd8e33a8cce2
Noesis Mik <sharp7c0timesfour@gmail.com>
2016-09-22 10:58:22 +0900

    confirm modal 추가

--- assets/scripts/new/components/contents/member.jsx
+++ assets/scripts/new/components/contents/member.jsx
@@ -2,13 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
+class ConfirmModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  show () {
+    this.setState({isOpen: true});
+  }
+
+  dismiss (applyOk = false) {
+    this.setState({isOpen: false});
+    if(applyOk) {
+      this.props.okCallback();
+    }
+  }
+
+  render () {
+    return <div className={"modal " + (this.state.isOpen ? "is-active" : "")}>
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <section className="modal-card-body">
+          Want a remove this member?
+        </section>
+        <footer className="modal-card-foot">
+          <button onClick={this.dismiss.bind(this, true)} type="button" className="button is-primary">Yes</button>
+          <button onClick={this.dismiss.bind(this, false)} type="button" className="button">No</button>
+        </footer>
+      </div>
+    </div>
+  }
+}
+
 class Member extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditing         : false
+    }
+  }
+
+  enterEdit() {
+    this.setState({isEditing: true});
+  }
+
+  exitEdit(applyEdit = false) {
+    this.setState({isEditing: false});
+    // if(applyEdit) {
+    //   const { dispatch } = this.props;
+    //   const node = ReactDOM.findDOMNode(this.refs.titleInput);
+    //   const text = node.value.trim();
+    //   dispatch(updateTitle(text));
+    // }
+  }
+
+  confirmRemove() {
+    this.refs.confRemove.show();
+  }
+
+  remove() {
+    
+  }
 
   render() {
     return  <div className="comp-member media">
       <div className="media-left">
         <p className="image is-128x128">
-          <img src="http://placehold.it/128x128"/>
+          <img src="http://placehold.it/128x128?text=photo"/>
         </p>
       </div>
 
@@ -57,13 +122,16 @@ class Member extends React.Component {
           </span>
           <span>edit</span>
         </button>&nbsp;
-        <button className="button is-small is-danger is-outlined">
+        <button onClick={(e) => this.confirmRemove(e)} className="button is-small is-danger is-outlined">
           <span className="icon is-small">
             <i className="fa fa-trash"></i>
           </span>
           <span>remove</span>
         </button>
       </div>
+
+      {/* confirm modal */}
+      <ConfirmModal ref="confRemove" okCallback={() => this.remove()}></ConfirmModal>
 
     </div>
   }
